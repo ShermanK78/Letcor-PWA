9a3183fb16e7cd72b7274794560ae1198ea3e199
ShermanK78 <info@gteonline.co.za>
2023-12-01 15:17:40 +0200

    Updated config.js to disable pwa  in production mode

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const prod = process.env.NODE_ENV === 'production'
+
+
 const withPWA = require('next-pwa')({
     dest: "public",
     register: true,
-    skipWaiting: true
+    skipWaiting: true,
+    disable: prod ? false : true
+
   })
   
   module.exports = withPWA({
