de93165c457256914e89410aae0967f1c41e29f9
ShermanK78 <info@gteonline.co.za>
2023-12-02 09:08:09 +0200

    Updated SW registration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const prod = process.env.NODE_ENV === 'production'
 
 
 const withPWA = require('next-pwa')({
     dest: "public",
     register: true,
     skipWaiting: true,
-    disable: prod ? false : true
 
   })
   
